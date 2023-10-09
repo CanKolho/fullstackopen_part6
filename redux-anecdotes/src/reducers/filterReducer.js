4a8b06ddc0bc71bbd7df17480ab79d2138ae7086
Can Kolho <can.kolho@aalto.fi>
2023-10-09 14:10:11 +0300

    DONE 6.9 paremmat anekdootit, step7

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -3,7 +3,7 @@ const initialState = ''
 const filterReducer = (state = initialState, action) => {
   switch (action.type) {
     case 'SET_FILTER':
-      return action.payload
+      return action.payload.filter
     default:
       return state
   }
