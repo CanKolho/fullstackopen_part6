4856674d453d74e6402e7e6754e677b6a9063cb8
Can Kolho <can.kolho@aalto.fi>
2023-10-09 15:44:18 +0300

    6.10 anekdootit, step8

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,19 +1,17 @@
+import { createSlice } from '@reduxjs/toolkit'
+
 const initialState = ''
 
-const filterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return action.payload.filter
-    default:
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setFilter(state, action) {
+      state = action.payload
       return state
+    }
   }
-}
+})
 
-export const filterChange = filter => {
- return {
-    type: 'SET_FILTER',
-    payload: { filter },
-  }
-}
-
-export default filterReducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
