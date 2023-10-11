9eff184bcc514a502861151cf78464099e1f2729
Can Kolho <can.kolho@hotmail.com>
2023-10-11 13:54:05 +0300

    6.14 anekdootit ja backend, step1

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import anecdoteService from './services/anecdotes'
+import { setAnecdotes } from './reducers/anecdoteReducer'
+
 import AnecdotesList from "./components/AnecdotesList"
 import AnecdotesForm from "./components/AnecdotesForm"
 import Filter from "./components/Filter"
 import Notification from "./components/Notification"
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then(anecdotes => dispatch(setAnecdotes(anecdotes)))
+  }, [])
+
   return (
     <>
       <h2>Anecdotes</h2>
